84da0ea14f3093965aabecdece1c83f54e8c1458
Sébastien Menard <sebastien.menard.github@gmail.com>
2023-09-04 13:44:52 -0400

    build(workflow): test workflow with github paths (#11)
    * build(workflow): test workflow with github paths
    
    * build(temp): test workflow call with python files
    
    * build(paths): test workflow call without python files
    
    * build(workflows): test workflows using the workflow_run conditions
    
    * build(workflows): add missing success condition
    
    * test(workflows): test workflows with update to python files
    
    * build(workflows): update ci with paths logic for python files
    
    * build(virtualenv): remove the --on-interaction flag

--- src/temp.py
+++ src/temp.py
@@ -1,6 +1,11 @@
 """Temp file."""
 
 
-def temp() -> bool:
-    """Return temp bool."""
+def temp_true() -> bool:
+    """Return true."""
     return True
+
+
+def temp_false() -> bool:
+    """Return false."""
+    return False
