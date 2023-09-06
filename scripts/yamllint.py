bb4223cc29059258092b629bfc17fc9cc33edb74
MenSeb <sebastien.menard.github@gmail.com>
2023-09-05 21:42:37 -0400

    build(yamllint): update script to replace file endings from crlf to lf

--- scripts/yamllint.py
+++ scripts/yamllint.py
@@ -32,13 +32,48 @@ def format_paths_exclude(paths: list[str]) -> str:
     return " --exclude=".join(map(format_path_glob, paths))
 
 
+def find_yaml_files() -> list[str]:
+    """Find YAML files using a glob pattern,."""
+    yml = list(map(str, Path().glob("**/*.yml")))
+    yaml = list(map(str, Path().glob("**/*.yaml")))
+    return yml + yaml
+
+
+def filter_yaml_file(file: str, paths: list[str]) -> bool:
+    """Return true if file starts with one of the paths."""
+    return any(file.startswith(path) for path in paths)
+
+
+def filter_yaml_files(files: list[str], paths: list[str]) -> list[str]:
+    """Filter YAML files with paths."""
+    str_paths = [str(Path(path)) for path in paths]
+    return list(
+        filter(lambda file: not filter_yaml_file(file, str_paths), files),
+    )
+
+
+def fix_yaml_file_endings(path: str) -> None:
+    """Fix YAML file endings from CRLF to LF."""
+    with Path.open(path, "rb") as file:
+        content = file.read()
+
+    content = content.replace(b"\r\n", b"\n")
+
+    with Path.open(path, "wb") as file:
+        file.write(content)
+
+
 def yamlfix_cli() -> None:
     """Call yamlfix from cli and exclude .gitignore paths."""
-    exclude = format_paths_exclude(filter_file_lines(read_file_lines(".gitignore")))
+    paths_gitignore = filter_file_lines(read_file_lines(".gitignore"))
+    exclude = format_paths_exclude(paths_gitignore)
+    yaml_files = filter_yaml_files(find_yaml_files(), paths_gitignore)
     subprocess.run(
         f"poetry run yamlfix . --exclude={exclude}",  # noqa: S603
         check=False,
     )
+    for yaml_file in yaml_files:
+        fix_yaml_file_endings(yaml_file)
 
 
 yamlfix_cli()
