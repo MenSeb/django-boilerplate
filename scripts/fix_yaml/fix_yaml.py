4e51924828f8b1fd76ae5f6857e7f415b6c4aaac
MenSeb <sebastien.menard.github@gmail.com>
2023-09-06 23:58:42 -0400

    build(yamlfix): update script yamlfix logic

--- scripts/fix_yaml/fix_yaml.py
+++ scripts/fix_yaml/fix_yaml.py
@@ -1,53 +1,30 @@
-"""Fix linting errors from YAML files and exclude .gitignore paths."""
-
+"""Fix linting errors from YAML files."""
 
 from __future__ import annotations
 
 import subprocess
+from itertools import chain
 from logging import INFO, basicConfig, info
 from pathlib import Path
-from typing import TextIO
+from typing import Iterable, TextIO
 
 basicConfig(level=INFO)
 
 
-def read_file_lines(path: str) -> list[str]:
-    """Read the lines from a file."""
-    return Path.open(path).read().split("\n")
+def find_yaml_files(source: str = ".") -> Iterable[Path]:
+    """Find YAML files from source using glob patterns."""
+    return chain(Path(source).glob("**/*.yml"), Path(source).glob("**/*.yaml"))
 
 
-def is_path_folder(line: str) -> bool:
-    """Return true if the path is a folder."""
-    return line.endswith("/") and not line.startswith("#")
+def filter_yaml_file(path: Path, gitignore: str) -> bool:
+    """Return true if the file is not found in gitignore."""
+    file = str(path)
+
+    return gitignore.find(file[: file.find("\\")]) == -1
 
 
-def filter_file_lines(lines: list[str]) -> list[str]:
-    """Filter the lines in file to keep the one as folder path."""
-    return list(filter(is_path_folder, lines))
-
-
-def find_yaml_files() -> list[str]:
-    """Find YAML files using a glob pattern,."""
-    yml = list(map(str, Path().glob("**/*.yml")))
-    yaml = list(map(str, Path().glob("**/*.yaml")))
-    return yml + yaml
-
-
-def filter_yaml_file(file: str, paths: list[str]) -> bool:
-    """Return true if file starts with one of the paths."""
-    return any(file.startswith(path) for path in paths)
-
-
-def filter_yaml_files(files: list[str], paths: list[str]) -> list[str]:
-    """Filter YAML files with paths."""
-    str_paths = [str(Path(path)) for path in paths]
-    return list(
-        filter(lambda file: not filter_yaml_file(file, str_paths), files),
-    )
-
-
-def fix_yaml_file_endings(path: str) -> None:
-    """Fix YAML file endings from CRLF to LF."""
+def fix_file_endings(path: str) -> None:
+    """Fix file endings from CRLF to LF."""
     file: TextIO
 
     with Path.open(path, "rb") as file:
@@ -61,24 +38,37 @@ def fix_yaml_file_endings(path: str) -> None:
     file.close()
 
 
+def run_yamlfix_file(path: str) -> str:
+    """Run yamlfix on path and return process stderr."""
+    return subprocess.run(
+        f"poetry run yamlfix {path}",  # noqa: S603
+        check=False,
+        capture_output=True,
+        text=True,
+    ).stderr
+
+
 def fix_yaml_files() -> None:
     """Fix all YAML files in the project.
 
     Find all YAML files in the project excluding paths from .gitignore.
 
     For each file:
-        Call yamlfix cli to fix errors.
+      Run yamlfix on file
 
-        Update line endings from CRLF to LF.
+      if file was fixed
+        Fix line endings from CRLF to LF.
     """
-    paths_gitignore = filter_file_lines(read_file_lines(".gitignore"))
-
-    yaml_files = filter_yaml_files(find_yaml_files(), paths_gitignore)
+    gitignore = Path.open(".gitignore").read()
+    yaml_files = filter(
+        lambda file: filter_yaml_file(file, gitignore),
+        find_yaml_files(),
+    )
 
     for yaml_file in yaml_files:
-        info(f"YamlFix - File : {yaml_file}")
-        subprocess.run(
-            f"poetry run yamlfix {yaml_file}",  # noqa: S603
-            check=False,
-        )
-        fix_yaml_file_endings(yaml_file)
+        result = run_yamlfix_file(yaml_file)
+
+        if result.find("1 fixed") != -1:
+            fix_file_endings(yaml_file)
+
+        info(f"Run yamlfix on file -> {yaml_file}\n{result}")
